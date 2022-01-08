e523449bb5a800461701bdc5e38614af0d43805f
maurogoyeneche <goyenechemauro@gmail.com>
2022-01-08 00:58:17 -0300

    api transaction sending json - ok

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,10 +1,10 @@
 const { Transaction } = require('../models');
 
-async function showHome(req, res) {
+async function index(req, res) {
   const transactions = await Transaction.findAll();
   res.json(transactions);
 }
 
 module.exports = {
-  showHome,
+  index,
 };
