3c6cf983a2c10c2012bbd8b51705c0614b98281a
maurogoyeneche <goyenechemauro@gmail.com>
2022-01-16 23:40:48 -0300

    habbemus login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,20 @@
-const { User } = require("../models");
+const { User } = require('../models');
+const jwt = require('jsonwebtoken');
 
-// Handlers para User...
-// ...
+const showLogin = (req, res) => {};
+
+const login = async (req, res) => {
+  const user = await User.findOne({ where: { email: req.body.email } });
+  console.log(user);
+  const token = jwt.sign({ id: user.id, email: user.email }, 'estaeslaclavesecreta');
+  if (user.password === req.body.password) {
+    res.json({ accesToken: token });
+  } else {
+    res.status(401).json({ error: 'Usuario o contraseña incorrectos' });
+  }
+};
+
+module.exports = {
+  showLogin,
+  login,
+};
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,7 +1,8 @@
-const express = require("express");
+const express = require('express');
 const publicRouter = express.Router();
+const userController = require('../controllers/userController');
 
 // Rutas del Públicas:
-// ...
+publicRouter.post('/token', userController.login);
 
 module.exports = publicRouter;
