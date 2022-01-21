f4869c8ae7b6a9f2ff1518b6efb8d63a8f32d821
maurogoyeneche <goyenechemauro@gmail.com>
2022-01-21 01:38:31 -0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,9 +5,11 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME, // Ej: root
   process.env.DB_PASSWORD, // Ej: root
   {
-    host: process.env.DB_HOST, // Ej: 127.0.0.1
-    dialect: process.env.DB_CONNECTION, // Ej: mysql
-    logging: false, // Para que no aparezcan mensajes en consola.
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION,
+    dialectModule: require('pg'),
+    omitNull: true,
+    logging: false,
   },
 );
 
