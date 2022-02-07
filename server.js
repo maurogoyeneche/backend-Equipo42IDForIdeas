6bf279acc52af7d4691282765df8c6e0e458f069
maurogoyeneche <goyenechemauro@gmail.com>
2022-02-07 00:11:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const routes = require('./routes');
 const dbInitialSetup = require('./dbInitialSetup');
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
+const cors = require('cors');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(cors());
 routes(app);
 
 //dbInitialSetup(); // Crea tablas e inserta datos de prueba.
